96a9cdf2156e3c3ad13834e911ea220194ba9f0e
ecarballo <ecarballo@sinprol.com.mx>
2023-06-01 16:50:03 -0600

    Estilos de appbar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  const AppTheme({
+    this.selectedColor = 0,
+  }): assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+      assert(selectedColor < colorList.length, 'Selected color must be less o equal than ${colorList.length - 1}');
+
+  ThemeData getTheme()  => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+    appBarTheme: const AppBarTheme(
+      centerTitle: false
+    )
+  );
+}
